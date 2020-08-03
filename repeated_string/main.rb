7070e42affc6ae5cfc91e14ff7d6149c60709283
Gui Recordon <guilherme.recordon@gmail.com>
2020-08-03 17:01:32 -0300

    Refactor main

--- repeated_string/main.rb
+++ repeated_string/main.rb
@@ -1,8 +1,7 @@
 def repeatedString(s, n)
    a1 = s.count("a")*(n/s.size)
-    
-   for i in 0...(n % s.size)
-     a1 += 1 if s[i] == "a"
-   end
+
+   a1 += s.split("").slice(0, n%s.size).count("a")
+   
    a1
 end
